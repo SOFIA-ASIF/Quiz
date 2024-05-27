66fb2ace02419fbf252339a9a8dbe274ad9c4f3d
SOFIA-ASIF <160203172+SOFIA-ASIF@users.noreply.github.com>
2024-05-27 07:50:38 -0700

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import chalk from 'chalk';
-import readlineSync from 'readline-sync';
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk_1 = __importDefault(require("chalk"));
+const readline_sync_1 = __importDefault(require("readline-sync"));
 const questions = [
     { question: 'What is the capital of Australia?', choices: ['Sydney', 'Melbourne', 'Brisbane', 'Canberra'], correctAnswer: 3 },
     { question: 'Which planet is known as the Red Planet?', choices: ['Earth', 'Mars', 'Jupiter', 'Venus'], correctAnswer: 1 },
@@ -12,24 +17,24 @@ const questions = [
     { question: 'Who painted the ceiling of the Sistine Chapel?', choices: ['Leonardo da Vinci', 'Michelangelo', 'Raphael', 'Donatello'], correctAnswer: 1 },
     { question: 'Which country is known as the Land of the Rising Sun?', choices: ['China', 'Japan', 'Thailand', 'South Korea'], correctAnswer: 1 }
 ];
-console.log(chalk.gray("WELCOME TO THE QUIZ GAME"));
-console.log(chalk.green(`Instructions:
+console.log(chalk_1.default.gray("WELCOME TO THE QUIZ GAME"));
+console.log(chalk_1.default.green(`Instructions:
 1. You have to write the number of choice you want to answer
 2. Each question is of 1 mark
 3. You will get total marks at the end of the quiz`));
 let score = 0;
 questions.forEach((q, index) => {
-    console.log(chalk.yellow(`Q${index + 1}: ${q.question}`));
+    console.log(chalk_1.default.yellow(`Q${index + 1}: ${q.question}`));
     q.choices.forEach((choice, i) => {
-        console.log(chalk.cyan(`${i + 1}. ${choice}`));
+        console.log(chalk_1.default.cyan(`${i + 1}. ${choice}`));
     });
-    const answer = readlineSync.questionInt('Your answer: ') - 1;
+    const answer = readline_sync_1.default.questionInt('Your answer: ') - 1;
     if (answer === q.correctAnswer) {
-        console.log(chalk.green('Correct!\n'));
+        console.log(chalk_1.default.green('Correct!\n'));
         score++;
     }
     else {
-        console.log(chalk.red('Incorrect!\n'));
+        console.log(chalk_1.default.red('Incorrect!\n'));
     }
 });
-console.log(chalk.blue(`Quiz finished! Your score is: ${score}/${questions.length}`));
+console.log(chalk_1.default.blue(`Quiz finished! Your score is: ${score}/${questions.length}`));
